04ae9a450b55ec2be1fb2af74242a9e11bc7866a
gegorov <gegorov@yahoo.com>
2019-09-09 14:33:32 +0300

    section 2 episode 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-basics';
+    title = 'angular-basics';
+    inputValue = '';
+
+    constructor() {}
+
+    onInput(event: KeyboardEvent) {
+        console.log('Event ', event);
+        this.inputValue = (event.target as HTMLInputElement).value;
+    }
+    onClick() {
+        console.log('Clicked!');
+    }
+    onBlur(str: string) {
+        this.inputValue = str;
+    }
 }
