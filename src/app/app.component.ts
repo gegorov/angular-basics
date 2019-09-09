b880438866d7eefe6e02d447b13cc5807e66d12a
gegorov <gegorov@yahoo.com>
2019-09-09 15:59:42 +0300

    section 2 ngif else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'aloha';
-    backgroundToggle = false;
+    toggle = false;
 
     onInput(event: any) {
         this.title = event.target.value;
