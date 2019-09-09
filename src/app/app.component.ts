81a86e31f2f98ac9ab35352e24d807fd20426560
gegorov <gegorov@yahoo.com>
2019-09-09 17:43:34 +0300

    section2 ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,24 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = 'aloha';
-    toggle: any = false;
-
-    onInput(event: any) {
-        this.title = event.target.value;
-    }
+    someStrangeArray = [1, 1, 2, 3, 5, 8, 13, 21];
+    someStrangeObjectsArray = [
+        {
+            title: 'Post1',
+            author: 'John Doe',
+            comments: [
+                { name: 'Donald Trump', text: 'my hands are the biggest' },
+                { name: 'Barak', text: 'lol' },
+                { name: 'Stormy Daniels', text: 'Pathetic...' },
+            ],
+        },
+        {
+            title: 'Post2',
+            author: 'Bob Marley',
+            comments: [
+                { name: 'Jane Dow', text: 'Lorem' },
+                { name: 'Stormy', text: 'lol-lol' },
+            ],
+        },
+    ];
 }
