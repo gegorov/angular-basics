a8f9eabdec571e1112d47f17ad132f1f5e2cda5d
gegorov <gegorov@yahoo.com>
2019-09-10 12:33:03 +0300

    add section 3 lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,22 +18,30 @@ export class AppComponent {
             text: 'It is a long process. I am still learning',
             id: 1,
         },
-        {
-            title: 'Next Post',
-            text:
-                'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dignissimos?',
-            id: 2,
-        },
-        {
-            title: 'Lipsum',
-            text:
-                'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex voluptate quisquam reprehenderit at eum repellendus!',
-            id: 3,
-        },
+        // {
+        //     title: 'Next Post',
+        //     text:
+        //         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dignissimos?',
+        //     id: 2,
+        // },
+        // {
+        //     title: 'Lipsum',
+        //     text:
+        //         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex voluptate quisquam reprehenderit at eum repellendus!',
+        //     id: 3,
+        // },
     ];
 
     updatePosts(post: Post) {
-        this.posts.unshift(post);
+        this.posts.unshift({
+            ...post,
+            id: this.posts[0] ? this.posts[0].id + 1 : 1,
+        });
         console.log('recived: ', post);
+    }
+
+    removePost(id: number) {
+        console.log('id to be removed: ', id);
+        this.posts = this.posts.filter(post => post.id !== id);
     }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Input,
+    ContentChild,
+    ElementRef,
+    OnChanges,
+    SimpleChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy,
+    Output,
+    EventEmitter,
+} from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -6,10 +22,59 @@ import { Post } from '../app.component';
     templateUrl: './post.component.html',
     styleUrls: ['./post.component.scss'],
 })
-export class PostComponent implements OnInit {
+// DoCheck,
+export class PostComponent
+    implements
+        OnInit,
+        OnChanges,
+        AfterContentInit,
+        AfterContentChecked,
+        AfterViewInit,
+        AfterViewChecked,
+        OnDestroy {
     @Input() post: Post;
+    @Output() onRemove = new EventEmitter<number>();
 
-    constructor() {}
+    @ContentChild('info', { static: true }) infoRef: ElementRef;
 
-    ngOnInit() {}
+    constructor() {
+        console.log('Constructor');
+    }
+
+    removePost() {
+        console.log('inside post component: ', this.post.id);
+        this.onRemove.emit(this.post.id);
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('ngOnChanges', changes);
+    }
+    ngOnInit() {
+        // console.log(this.infoRef.nativeElement);
+        console.log('ngOnInit');
+    }
+
+    // ngDoCheck(): void {
+    //     console.log('ngDoCheck');
+    // }
+
+    ngAfterContentInit() {
+        console.log('afterContentinit');
+    }
+
+    ngAfterContentChecked() {
+        console.log('ngAfterContentChecked()');
+    }
+
+    ngAfterViewInit() {
+        console.log('ngAfterViewInit()');
+    }
+
+    ngAfterViewChecked() {
+        console.log('ngAfterViewChecked');
+    }
+
+    ngOnDestroy() {
+        console.log('ngOnDestroy()');
+    }
 }
