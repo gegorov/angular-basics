89dcd18fa9fc7c8894fcf54c3e5e4eeefe3e9861
gegorov <gegorov@yahoo.com>
2019-09-10 16:27:56 +0300

    add section 4 Derectives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Post {
-    title: string;
-    text: string;
-    id?: number;
-}
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    posts: Post[] = [
-        {
-            title: 'Learning Angular components',
-            text: 'It is a long process. I am still learning',
-            id: 1,
-        },
-        // {
-        //     title: 'Next Post',
-        //     text:
-        //         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dignissimos?',
-        //     id: 2,
-        // },
-        // {
-        //     title: 'Lipsum',
-        //     text:
-        //         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex voluptate quisquam reprehenderit at eum repellendus!',
-        //     id: 3,
-        // },
-    ];
+    isVisible = true;
 
     ngOnInit(): void {}
-
-    updatePosts(post: Post) {
-        this.posts.unshift({
-            ...post,
-            id: this.posts[0] ? this.posts[0].id + 1 : 1,
-        });
-        console.log('recived: ', post);
-    }
-
-    removePost(id: number) {
-        console.log('id to be removed: ', id);
-        this.posts = this.posts.filter(post => post.id !== id);
-    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
+import { StyleDirective } from './directives/style.derective';
+import { IfnotDirective } from './directives/ifnot.directive';
 
 @NgModule({
-    declarations: [AppComponent, PostFormComponent, PostComponent],
+    declarations: [AppComponent, StyleDirective, IfnotDirective],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
