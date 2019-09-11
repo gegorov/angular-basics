b62ed4287989de5a8836b55f89167d91bf47445b
gegorov <gegorov@yahoo.com>
2019-09-12 00:10:06 +0300

    add custom ngmodel implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
-import { MyValidators } from './my.validators';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    form: FormGroup;
+export class AppComponent {
+    appState = 'on';
 
-    ngOnInit(): void {
-        this.form = new FormGroup({
-            email: new FormControl(
-                '',
-                [Validators.email, Validators.required, MyValidators.restrictedEmails],
-                [MyValidators.uniqEmail]
-            ),
-            password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
-            address: new FormGroup({
-                country: new FormControl('ru'),
-                city: new FormControl('', Validators.required),
-            }),
-            skills: new FormArray([]),
-        });
-    }
-    addSkill() {
-        const control = new FormControl('', Validators.required);
-
-        (this.form.get('skills') as FormArray).push(control);
-    }
-    setCapital() {
-        const cityMap = {
-            ru: 'Moskvabad',
-            by: 'Minsk',
-            ua: 'Kiyv',
-        };
-        const cityKey = this.form.get('address').get('country').value;
-        const city = cityMap[cityKey];
-
-        console.log('City: ', city);
-        this.form.patchValue({
-            address: { city },
-        });
-    }
-
-    submit() {
-        if (this.form.valid) {
-            console.log('Form!', this.form);
-            const formData = { ...this.form.value };
-
-            console.log('Form Data: ', formData);
-
-            this.form.reset();
-        }
+    handleChange() {
+        console.log(this.appState);
     }
 }
---
+++ src/app/switch/switch.component.ts
@@ -0,0 +1,38 @@
+import { Component, Provider, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+const VALUE_ACCESSOR: Provider = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => SwitchComponent),
+    multi: true,
+};
+
+@Component({
+    selector: 'app-switch',
+    templateUrl: './switch.component.html',
+    styleUrls: ['./switch.component.scss'],
+    providers: [VALUE_ACCESSOR],
+})
+export class SwitchComponent implements ControlValueAccessor {
+    state = 'off';
+
+    private onChange = (value: any) => {};
+
+    setState(state: string) {
+        this.state = state;
+        this.onChange(this.state);
+    }
+    writeValue(state: string): void {
+        // throw new Error('Method not implemented.');
+        this.state = state;
+    }
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+    registerOnTouched(fn: any): void {
+        // throw new Error('Method not implemented.');
+    }
+    setDisabledState?(isDisabled: boolean): void {
+        // throw new Error('Method not implemented.');
+    }
+}
