9ccc485f3c31768ab1616f91bbd61584ca17d9e3
gegorov <gegorov@yahoo.com>
2019-09-11 11:48:39 +0300

    add cusotom pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Post {
+    title: string;
+    text: string;
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-    isVisible = true;
+export class AppComponent {
+    searchQuery = '';
 
-    ngOnInit(): void {}
+    posts: Post[] = [
+        {
+            title: 'magic is happening',
+            text:
+                'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, quia?',
+        },
+        {
+            title: 'This is kind of a Magic',
+            text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum nostrum possimus
+                autem vitae est veritatis adipisci molestiae voluptates ex suscipit.`,
+        },
+        {
+            title: 'The show must go on',
+            text: `
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis dolores repellat
+                facilis totam iusto odio ipsam, temporibus libero soluta quasi autem nihil
+                architecto vitae nemo aspernatur at rem eum vel quo nulla sed impedit modi beatae.
+                Itaque labore, aliquid voluptates necessitatibus iusto praesentium ipsum omnis
+                accusantium placeat doloribus. Quia, totam?
+                `,
+        },
+        {
+            title: 'Radio ga ga',
+            text: `
+                I'd sit alone and watch your light
+                My only friend through teenage nights
+                And everything I had to know
+                I heard it on my radio`,
+        },
+    ];
+
+    addPost() {
+        this.posts.unshift({
+            title: 'angular',
+            text: 'blablalbal blabla lab lab',
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { StyleDirective } from './directives/style.derective';
-import { IfnotDirective } from './directives/ifnot.directive';
+import { MultByPipe } from './pipes/mult-by.pipe';
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-    declarations: [AppComponent, StyleDirective, IfnotDirective],
+    declarations: [AppComponent, MultByPipe, ExMarksPipe, FilterPipe],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
