67586be763f93d61cca3dbf7d4e0207c941b2e5f
gegorov <gegorov@yahoo.com>
2019-09-11 23:31:09 +0300

    add reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { MyValidators } from './my.validators';
 
 @Component({
     selector: 'app-root',
@@ -11,12 +12,36 @@ export class AppComponent implements OnInit {
 
     ngOnInit(): void {
         this.form = new FormGroup({
-            email: new FormControl('', [Validators.email, Validators.required]),
+            email: new FormControl(
+                '',
+                [Validators.email, Validators.required, MyValidators.restrictedEmails],
+                [MyValidators.uniqEmail]
+            ),
             password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
             address: new FormGroup({
                 country: new FormControl('ru'),
                 city: new FormControl('', Validators.required),
             }),
+            skills: new FormArray([]),
+        });
+    }
+    addSkill() {
+        const control = new FormControl('', Validators.required);
+
+        (this.form.get('skills') as FormArray).push(control);
+    }
+    setCapital() {
+        const cityMap = {
+            ru: 'Moskvabad',
+            by: 'Minsk',
+            ua: 'Kiyv',
+        };
+        const cityKey = this.form.get('address').get('country').value;
+        const city = cityMap[cityKey];
+
+        console.log('City: ', city);
+        this.form.patchValue({
+            address: { city },
         });
     }
 
@@ -26,6 +51,8 @@ export class AppComponent implements OnInit {
             const formData = { ...this.form.value };
 
             console.log('Form Data: ', formData);
+
+            this.form.reset();
         }
     }
 }
