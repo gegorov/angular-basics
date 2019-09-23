ec9b736fee82bb0977288207a9298351ab25504a
gegorov <gegorov@yahoo.com>
2019-09-23 18:09:34 +0300

    add basic operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,27 @@
-import {Component} from '@angular/core'
-import {interval, Subscription} from 'rxjs'
-// import {} from 'rxjs/operators'
+import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    sub: Subscription;
 
-  sub: Subscription
+    constructor() {
+        const intervalStream$ = interval(1000).pipe(
+            filter(value => value % 2 === 0),
+            map(value => `mapped value: ${value}`)
+        );
 
-  constructor() {
+        this.sub = intervalStream$.subscribe(value => {
+            console.log(value);
+        });
+    }
 
-    const intervalStream$ = interval(1000)
-
-    this.sub = intervalStream$.subscribe((value) => {
-      console.log(value)
-    })
-
-  }
-
-
-  stop() {
-    this.sub.unsubscribe()
-  }
+    stop() {
+        this.sub.unsubscribe();
+    }
 }
-
