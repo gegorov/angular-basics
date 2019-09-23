499c7c43001a236f0fe0b50bcf508b91696659b3
gegorov <gegorov@yahoo.com>
2019-09-23 13:13:10 +0300

    add routing with params

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { PostsService, Post } from '../posts.service';
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+    selector: 'app-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+    public post: Post;
+    constructor(
+        private route: ActivatedRoute,
+        private postsService: PostsService,
+        private router: Router
+    ) {}
+    ngOnInit() {
+        this.route.params.subscribe((params: Params) => {
+            console.log('Params: ', params);
+            this.post = this.postsService.getById(parseInt(params.id, 10));
+        });
+    }
 
+    loadPost() {
+        this.router.navigate(['/posts', 44]);
+    }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,29 @@
-import {Component} from '@angular/core'
-import {PostsService} from '../posts.service'
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+    selector: 'app-posts',
+    templateUrl: './posts.component.html',
+    styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent {
-  constructor(private postsService: PostsService) {}
+export class PostsComponent implements OnInit {
+    showIds = false;
+    constructor(
+        public postsService: PostsService,
+        public route: ActivatedRoute,
+        public router: Router
+    ) {}
+    ngOnInit() {
+        this.route.queryParams.subscribe((params: Params) => {
+            this.showIds = !!params.showIds;
+        });
+
+        this.route.fragment.subscribe(fragment => {
+            console.log('Fragment: ', fragment);
+        });
+    }
+    showIdsProgram() {
+        this.router.navigate(['/posts'], { queryParams: { showIds: true }, fragment: 'program' });
+    }
 }
