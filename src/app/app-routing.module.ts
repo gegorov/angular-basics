860de3b70c88df34371b15d32d2cfa508867d7ee
gegorov <gegorov@yahoo.com>
2019-09-23 16:02:51 +0300

    add modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,24 @@
-import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
-import {HomePageComponent} from './home-page/home-page.component'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
+import { NgModule } from '@angular/core';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
+const routes = [
+    {
+        path: '',
+        component: HomePageComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: 'about',
+        loadChildren: './about-page/aboute-page.module#AboutPageModule',
+    },
+];
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: 'about', component: AboutPageComponent, children: [
-      {path: 'extra', component: AboutExtraPageComponent}
-    ]},
-    {path: '', component: HomePageComponent, pathMatch: 'full'}
-  ])],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {
+            preloadingStrategy: PreloadAllModules,
+        }),
+    ],
+    exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
